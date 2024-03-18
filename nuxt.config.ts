410b91b8bd49485486f5ec67ab689481a4b72ac5
lucas2502 <lucas2502@live.com>
2024-03-18 08:56:51 -0300

    chore: pull origin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
@@ -49,3 +50,108 @@ export default defineNuxtConfig({
     }
   }
 })
+||||||| merged common ancestors
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  srcDir: 'src/',
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'FF Boilerplate'
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@pinia/nuxt',
+    [
+      '@nuxtjs/i18n',
+      {
+        vueI18n: './i18n.config.ts'
+      }
+    ]
+  ],
+  pinia: {
+    storesDirs: ['./stores/**']
+  },
+  piniaPersistedstate: {
+    cookieOptions: {
+      sameSite: 'strict'
+    },
+    storage: 'localStorage'
+  },
+  devtools: {
+    enabled: true,
+    timeline: { enabled: true }
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: tag =>
+        tag.startsWith('ff-')
+    }
+  },
+  runtimeConfig: {
+    public: {
+      api: {
+        apiBaseUrl:
+          process.env.API_BASE_URL || '',
+        isMockActive:
+          process.env.IS_MOCK_ACTIVE === 'true'
+      }
+    }
+  }
+})
+=======
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  srcDir: 'src/',
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'FF Boilerplate'
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@pinia/nuxt',
+    [
+      '@nuxtjs/i18n',
+      {
+        vueI18n: './i18n.config.ts'
+      }
+    ]
+  ],
+  pinia: {
+    storesDirs: ['./stores/**']
+  },
+  piniaPersistedstate: {
+    cookieOptions: {
+      sameSite: 'strict'
+    },
+    storage: 'localStorage'
+  },
+  devtools: {
+    enabled: true,
+    timeline: { enabled: true }
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: tag =>
+        tag.startsWith('ff-')
+    }
+  },
+  runtimeConfig: {
+    public: {
+      api: {
+        apiBaseUrl:
+          process.env.API_BASE_URL || '',
+        isMockActive:
+          process.env.IS_MOCK_ACTIVE === 'true'
+      }
+    }
+  }
+})
+>>>>>>> fix: useRuntimeCOnfig
--- src/logic/services/index.ts
+++ src/logic/services/index.ts
@@ -1,7 +1,9 @@
 import { HttpAdapter } from '../adapter/HttpAdapter'
 import { UserService } from './user'
 import { UserServiceMock } from './user/mock'
+
 const config = useRuntimeConfig()
+
 const MOCK = config.public.api.isMockActive
 
 const httpAdapter = new HttpAdapter()
