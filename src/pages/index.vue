5a30e71cd9d753365d753d575d7fd6d28dcd4829
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-03-18 14:31:01 -0300

    list staged

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,28 +10,28 @@
   </ff-container>
 </template>
 <script>
-import { userService } from '~/logic/services'
+  import { userService } from '~/logic/services'
 
-export default {
-  async mounted() {
-    const data = await userService.getUser()
-    console.log(data)
-  },
-  methods: {
-    async getUser() {
+  export default {
+    async mounted() {
       const data = await userService.getUser()
       console.log(data)
+    },
+    methods: {
+      async getUser() {
+        const data = await userService.getUser()
+        console.log(data)
+      }
     }
   }
-}
 </script>
 <style lang="scss" scoped>
-.button {
-  color: red;
-  &:hover {
-    color: blue;
+  .button {
+    color: red;
+    &:hover {
+      color: blue;
+    }
   }
-}
 </style>
 <!-- <script setup>
 import { useUserStore } from '@/store/user'
