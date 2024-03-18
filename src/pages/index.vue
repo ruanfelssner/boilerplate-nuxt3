59583d3e10672a1020ec40f6b39286eb2d83db3e
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-03-18 14:29:37 -0300

    editor config, eslint prettier configs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,9 +4,9 @@
       button-text="Primary"
       size="medium"
       type="primary"
-    />
-
-    <h2 class="teste">Teste</h2>
+    >
+      <h2>teste do teste</h2>
+    </ff-button>
   </ff-container>
 </template>
 <script>
@@ -16,9 +16,23 @@ export default {
   async mounted() {
     const data = await userService.getUser()
     console.log(data)
+  },
+  methods: {
+    async getUser() {
+      const data = await userService.getUser()
+      console.log(data)
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.button {
+  color: red;
+  &:hover {
+    color: blue;
+  }
+}
+</style>
 <!-- <script setup>
 import { useUserStore } from '@/store/user'
 const store = useUserStore()
